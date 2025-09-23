779894f7f8fd234eff997ab7e0902d7f2b7579b7
TejasriBompada <tejasribompada97@gmail.com>
2025-09-23 12:42:07 -0400

    docs(tf): add comment indicating ALB is public in SG

--- modules/security_groups/main.tf
+++ modules/security_groups/main.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "public_sg" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # ALB public  
   }
 
   egress {
